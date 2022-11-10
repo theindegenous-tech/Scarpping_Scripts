7850d10d8d147cbbe1fb213470515837c858d0fa
jagrit-Indegenous <jagrit@theindegenous.org>
2022-11-11 02:25:31 +0530

    file upload t1

--- archive_1.py
+++ archive_1.py
@@ -1,8 +1,47 @@
 
+from sqlite3 import Cursor
+import psycopg2
 import requests
 from bs4 import BeautifulSoup
 
 
+from boto3 import session
+from botocore.client import Config
+
+ACCESS_ID = 'DO00DZN9YDVM4H4QKLPE'
+SECRET_KEY = 'TJZGQgbfYS7aES8D9H/h5+/hDutOyn8BGwfhRmSdc1c'
+
+# Initiate session
+session = session.Session()
+client = session.client('s3',
+                        region_name='nyc3',
+                        endpoint_url='https://nyc3.digitaloceanspaces.com',
+                        aws_access_key_id=ACCESS_ID,
+                        aws_secret_access_key=SECRET_KEY)
+						
+client.upload_file('pdf9.pdf', 'adding_books', 'pdf9.pdf')
+
+
+def connectSQL():
+		try:
+			connection = psycopg2.connect(user="postgres",
+										password="admin",
+										host="127.0.0.1",
+										port="5432",
+										database="library")
+			cursor = connection.cursor()
+			postgres_insert_query = "INSERT INTO the_indegenous_backend_book_clone VALUES  (2,2,'Test',1,'desc',12,'Test','Test','Test','Test','Test','Test','')"
+			# record_to_insert = ()
+			cursor.execute(postgres_insert_query)
+
+			connection.commit()
+			count = cursor.rowcount
+			print(count, "Record inserted successfully into mobile table")
+
+		except (Exception, psycopg2.Error) as error:
+			print("Failed to insert record into mobile table", error)
+		
+connectSQL()
 # URL from which pdfs to be downloaded
 url = "https://archive.org/details/cdl"
 
@@ -35,7 +74,6 @@ for link in links:
 	# Find all hyperlinks present on webpage
 	links_2 = soup.find_all(class_ = "format-summary download-pill")
 
-	list = ["other str", 2, 56]
 	for hit in links_2:
 			i=i+1
 			hit_detail = hit.text.strip()	
@@ -46,13 +84,45 @@ for link in links:
 						comp_url =( "https://archive.org"+hit['href'])
 						print("comp_url",comp_url)
 						response = requests.get(comp_url)
-
+						name = "pdf"+str(i)+".pdf"
 						pdf = open("pdf"+str(i)+".pdf", 'wb')
 						# print("response.content",response.content)
 						pdf.write(response.content)
 
 						pdf.close()
 						print("File ", i, " downloaded")
+
+					# Initiate session
+						session = session.Session()
+						client = session.client('s3',
+												region_name='nyc3',
+												endpoint_url='https://nyc3.digitaloceanspaces.com',
+												aws_access_key_id=ACCESS_ID,
+												aws_secret_access_key=SECRET_KEY)
+												
+						# client.upload_file(name)
+						print("File ",name , " uploaded")
+						
+
+						# title_list = article_list = author_list = list()
+
+						# for title in soup.find_all('h1',class_='post-title'):
+						# 	title_list.append(title)
+
+						# for article in soup.find_all(class_='post-content'):
+						# 	article_list.append(article)
+
+						# for author in soup.find_all(class_='author-name'):
+						# 	author_list.append(author)
+
+						# for index in range(0, len(title_list)):
+						# 	title = title_list[index]
+						# 	article = article_list[index]
+						# 	author = author_list[index]
+
+						# 	postgres_insert_query = """ INSERT INTO library (title_list, article_list, author_list) VALUES (%s,%s,%s)"""
+						# 	record_to_insert = (title,article,author)
+						# 	Cursor.execute(postgres_insert_query, record_to_insert)
 					except:
 						pass
 
