dbefa6139736cf2de4a319bb535d1acb1a56280c
jagrit-Indegenous <jagrit@theindegenous.org>
2022-11-15 20:45:43 +0530

    code for pdf updated

--- archive_1.py
+++ archive_1.py
@@ -40,107 +40,100 @@ def connectSQL(ID,title,url):
 # connectSQL(111,"title","https://booksdatabaseepub.nyc3.digitaloceanspaces.com/{filename}")
 
 # URL from which pdfs to be downloaded
-url = "https://archive.org/details/books?and%5B%5D=lending___status%3A%22is_readable%22&and%5B%5D=collection%3A%22americana%22&sort=-week&page=2"
+
+for i in range(100):
+	url = f"https://archive.org/details/books?and%5B%5D=lending___status%3A%22is_readable%22&and%5B%5D=collection%3A%22americana%22&sort=-week&page={i}"
 
 
-session = session.Session()
+	session = session.Session()
 
-# Requests URL and get response object
-response = requests.get(url,timeout=15) 
+	# Requests URL and get response object
+	response = requests.get(url,timeout=15) 
 
-# Parse text obtained
-soup = BeautifulSoup(response.text, 'html.parser')
+	# Parse text obtained
+	soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find all hyperlinks present on webpage
-links = soup.find_all(class_ = "item-ttl C C2")
+	# Find all hyperlinks present on webpage
+	links = soup.find_all(class_ = "item-ttl C C2")
 
-i = 0
+	i = 0
 
-# From all links check for pdf link and
-# if present download file
-for link in links:
-	
-	# print(link)
-	try:
-		ans = link.find('a')['href']
-		print("ans",ans)
-		filename = ans.split('/')[-1]
-		print("filename",filename)
+	# From all links check for pdf link and
+	# if present download file
+	for link in links:
+		
+		# print(link)
+		try:
+			ans = link.find('a')['href']
+			print("ans",ans)
+			filename = ans.split('/')[-1]
+			print("filename",filename)
 
-		comp_url =( "https://archive.org/"+ans)
+			comp_url =( "https://archive.org/"+ans)
 
-		# data_type =link.get('data-category')
-		response_2 = requests.get(comp_url,timeout=15) 
-			# Parse text obtained
-		soup = BeautifulSoup(response_2.text, 'html.parser')
-		# Find all hyperlinks present on webpage
-		links_2 = soup.find_all(class_ = "format-summary download-pill")
-		title = soup.find(class_ = "breaker-breaker").text
-		print("title",title)
-	# except:
-	# 	break
-		for hit in links_2:
-    			
-				i=i+1
-				hit_detail = hit.text.strip()	
-				if(hit_detail == "EPUB              download"):
-						try:
-							# print("yes inside",hit['href'])
-							
-							comp_url =( "https://archive.org"+hit['href'])
-							print("comp_url",comp_url)
-							response = requests.get(comp_url,timeout=15) 
-							path = "books/"+filename+".epub"
-							name_book = filename+".epub"
-							
-							idd = ''.join(str(random.randint(0,10)) for x in range(6))
-							print("idd",idd)
-
-							pdf = open("books/"+name_book, 'wb')
-							# print("response.content",response.content)
-							pdf.write(response.content)
-
-							pdf.close()
-							print("File ", i, " downloaded")
-
-						# Initiate session
+			# data_type =link.get('data-category')
+			response_2 = requests.get(comp_url,timeout=15) 
+				# Parse text obtained
+			soup = BeautifulSoup(response_2.text, 'html.parser')
+			# Find all hyperlinks present on webpage
+			links_2 = soup.find_all(class_ = "format-summary download-pill")
+			title = soup.find(class_ = "breaker-breaker").text
+			print("title",title)
+		# except:
+		# 	break
+			for hit in links_2:
+					
+					i=i+1
+					hit_detail = hit.text.strip()	
+					if(hit_detail == "PDF              download"):
 							try:
+								# print("yes inside",hit['href'])
 								
-								client = session.client('s3',
-														region_name='nyc3',
-														endpoint_url='https://nyc3.digitaloceanspaces.com',
-														aws_access_key_id=ACCESS_ID,
-														aws_secret_access_key=SECRET_KEY)
-														
-								# client.upload_file(name)
-								client.upload_file(path,  # Path to local file
-								'booksdatabaseepub',  # Name of Space
-								name_book)  # Name for remote file
-								connectSQL(idd,title,f'https://booksdatabaseepub.nyc3.digitaloceanspaces.com/{name_book}')
+								comp_url =( "https://archive.org"+hit['href'])
+								print("comp_url",comp_url)
+								response = requests.get(comp_url,timeout=15) 
+								path = "books/"+filename+".pdf"
+								name_book = filename+".pdf"
+								
+								idd = ''.join(str(random.randint(0,10)) for x in range(6))
+								print("idd",idd)
 
-								print("File ",filename , " uploaded")
+								pdf = open("books/"+name_book, 'wb')
+								# print("response.content",response.content)
+								pdf.write(response.content)
+
+								pdf.close()
+								print("File ", i, " downloaded")
+
+							# Initiate session
+								try:
+									
+									client = session.client('s3',
+															region_name='nyc3',
+															endpoint_url='https://nyc3.digitaloceanspaces.com',
+															aws_access_key_id=ACCESS_ID,
+															aws_secret_access_key=SECRET_KEY)
+															
+									# client.upload_file(name)
+									client.upload_file(path,  # Path to local file
+									'booksdatabaseepub',  # Name of Space
+									name_book)  # Name for remote file
+									connectSQL(idd,title,f'https://booksdatabaseepub.nyc3.digitaloceanspaces.com/{name_book}')
+
+									print("File ",filename , " uploaded")
+								except Exception as ex:
+									template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+									message = template.format(type(ex).__name__, ex.args)
+									print (message)
+
+								
 							except Exception as ex:
 								template = "An exception of type {0} occurred. Arguments:\n{1!r}"
 								message = template.format(type(ex).__name__, ex.args)
 								print (message)
 
-							
-						except Exception as ex:
-							template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-							message = template.format(type(ex).__name__, ex.args)
-							print (message)
-
-	except Exception as ex:
-		
-		template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-		message = template.format(type(ex).__name__, ex.args)
-		print (message)
-
-
-
-	# print("All PDF files downloaded")
-
-
-
-
-# print("All PDF files downloaded")
+		except Exception as ex:
+			
+			template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+			message = template.format(type(ex).__name__, ex.args)
+			print (message)
