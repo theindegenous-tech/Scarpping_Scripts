f9079e5b8315703d59f71f15471407acac858606
jagrit-Indegenous <jagrit@theindegenous.org>
2022-11-30 13:29:30 +0530

    new script with archive url

--- archive_3.py
+++ archive_3.py
@@ -62,7 +62,7 @@ for i in range(1000):
 		
 		# print(link)
 		try:
-			if(skip>1500):
+			if(skip>1):
 				ans = link.find('a')['href']
 				print("ans",ans)
 				filename = ans.split('/')[-1]
@@ -88,21 +88,24 @@ for i in range(1000):
 									
 									comp_url =( "https://archive.org"+hit['href'])
 									print("comp_url",comp_url)
-									response = requests.get(comp_url,timeout=25) 
-									path = "mnt/volume_nyc3_01/books/"+filename+".pdf"
-									name_book = filename+".pdf"
+									# response = requests.get(comp_url,timeout=25) 
+									# path = "mnt/volume_nyc3_01/books/"+filename+".pdf"
+									# name_book = filename+".pdf"
+
 									# idd = ''.join(str(random.randint(0,10)) for x in range(6))
 									# print("idd",idd)
 									# path_book = f'D:/books_pdf/{name_book}'
-									if(1):
-										pdf = open("mnt/volume_nyc3_01/books/"+name_book, 'wb')
-										# print("response.content",response.content)
-										pdf.write(response.content)
 
-										pdf.close()
+										# pdf = open("mnt/volume_nyc3_01/books/"+name_book, 'wb')
+										# # print("response.content",response.content)
+										# pdf.write(response.content)
 
-										connectSQL(title,comp_url,f'https://booksdatabaseepub.nyc3.digitaloceanspaces.com/books_pdf/{name_book}')
-										print("File ",filename , " uploaded")
+										# pdf.close()
+
+									# connectSQL(title,comp_url,f'https://booksdatabaseepub.nyc3.digitaloceanspaces.com/books_pdf/{name_book}')
+									connectSQL(title,comp_url,comp_url)
+
+									print("File ",title , " uploaded")
 
 									# Initiate session
 										# try:
@@ -123,10 +126,7 @@ for i in range(1000):
 										# 	exc_type, exc_obj, exc_tb = sys.exc_info()
 										# 	message = template.format(type(ex).__name__, ex.args)
 										# 	print (message,exc_tb.tb_lineno)
-									else:
-										print("book exist")
 
-									
 								except Exception as ex:
 									template = "An exception of type {0} occurred. Arguments:\n{1!r}"
 									exc_type, exc_obj, exc_tb = sys.exc_info()
